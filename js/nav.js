5f9fae484859458758825bc5c7e91ed5b3da394a
CesarWatrin <cesar.watrin@hotmail.com>
2020-12-18 10:57:21 +0100

    navbar

--- js/nav.js
+++ js/nav.js
@@ -1,5 +1,15 @@
 const burger = document.querySelector('.burger');
+const nav = document.querySelector('nav');
+const a = document.getElementsByTagName('a');
 
 burger.addEventListener('click', () => {
    burger.classList.toggle('active');
+   nav.classList.toggle('active');
 });
+
+for (var i = 0; i < a.length; i++) {
+   a[i].addEventListener('click', () => {
+      burger.classList.toggle('active');
+      nav.classList.toggle('active');
+   });
+}

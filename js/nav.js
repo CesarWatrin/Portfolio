d951aa8f3cd652754c7005a74f0d3ed01fa86ba4
CesarWatrin <cesar.watrin@hotmail.com>
2020-12-18 12:23:29 +0100

    mobile 100%

--- js/nav.js
+++ js/nav.js
@@ -8,8 +8,21 @@ burger.addEventListener('click', () => {
 });
 
 for (var i = 0; i < a.length; i++) {
-   a[i].addEventListener('click', () => {
-      burger.classList.toggle('active');
-      nav.classList.toggle('active');
-   });
+   if (a[i].className === 'toScroll nav') {
+      a[i].addEventListener('click', () => {
+         burger.classList.toggle('active');
+         nav.classList.toggle('active');
+      });
+   }
 }
+
+//scroll animation
+$("a.toScroll").on('click',function(e) {
+   var scrollSpeed = 1000;
+   var url = e.target.href;
+   var hash = url.substring(url.indexOf("#")+1);
+   $('html, body').animate({
+      scrollTop: $('#'+hash).offset().top
+   }, scrollSpeed);
+   return false;
+});

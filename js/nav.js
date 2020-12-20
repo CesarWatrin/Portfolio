94485c4c2d3a8540b258877d902a27d9c2764fe6
CesarWatrin <cesar.watrin@hotmail.com>
2020-12-20 12:45:17 +0100

    scroll animation

--- js/nav.js
+++ js/nav.js
@@ -49,3 +49,23 @@ AOS.init({
   mirror: false, // whether elements should animate out while scrolling past them
   anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
 });
+
+const progressBar = document.querySelector('.progress_bar');
+progressBar.style.display = "none";
+
+window.addEventListener('scroll', handleScroll);
+
+function handleScroll() {
+  progressBar.style.display = "block";
+  const height = document.body.scrollHeight; // taille du site
+  const windowHeight = window.innerHeight; // taille de l'affichage
+  const position = window.pageYOffset; // la position en pixels du document
+
+  const trackLength = height - windowHeight; // taille du site - la partie affichée sur l'écran en ce moment (exemple : 1000px)
+
+  const percentage =
+        Math.floor((position / trackLength) * 100); // pourcentage du site déjà parcouru
+
+  progressBar
+      .style.right = 100 - percentage + '%';
+}

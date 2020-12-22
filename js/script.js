bbb951b646dc39bbfe67bcb685b2c4984e5bb18b
CesarWatrin <cesar.watrin@hotmail.com>
2020-12-23 00:49:52 +0100

    test notif

--- js/script.js
+++ js/script.js
@@ -107,17 +107,23 @@ function onMouseHoverOut() {
    title_pointer.style.display = 'none';
 }
 
-// $(".form").submit(function(e) {
-//    e.preventDefault(); //empêcher une action par défaut
-//    var form_url = $(this).attr("action"); //récupérer l'URL du formulaire
-//    var form_method = $(this).attr("method"); //récupérer la méthode GET/POST du formulaire
-//    var form_data = $(this).serialize(); //Encoder les éléments du formulaire pour la soumission
-//
-//    $.ajax({
-//       url : form_url,
-//       type: form_method,
-//       data : form_data
-//    }).done(function(response){
-//       console.log('send');
-//    });
-// });
+const toast = document.querySelector('#notification');
+
+$(".form").submit(function(e) {
+   e.preventDefault(); //empêcher une action par défaut
+   var form_url = $(this).attr("action"); //récupérer l'URL du formulaire
+   var form_method = $(this).attr("method"); //récupérer la méthode GET/POST du formulaire
+   var form_data = $(this).serialize(); //Encoder les éléments du formulaire pour la soumission
+
+   $.ajax({
+      url : form_url,
+      type: form_method,
+      data : form_data
+   }).done(function(response){
+      console.log('send');
+      toast.classList.add("show");
+      setTimeout(() => {
+         toast.classList.remove("show");
+      }, 2000);
+   });
+});
